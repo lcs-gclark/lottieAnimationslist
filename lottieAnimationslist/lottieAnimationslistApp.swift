49354d4b7c159531524fde40fe68e44f50dc4443
grayden clark <gclark@lcs.on.ca>
2024-02-02 09:40:23 -0500

    Initial Commit

---
+++ lottieAnimationslist/lottieAnimationslistApp.swift
@@ -0,0 +1,17 @@
+//
+//  lottieAnimationslistApp.swift
+//  lottieAnimationslist
+//
+//  Created by Gray Clark on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct lottieAnimationslistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
